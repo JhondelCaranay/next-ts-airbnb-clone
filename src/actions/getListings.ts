c164ff8057a0b6a3a92fdeb82a09955d6ed7fa6b
jhondel <jhondeldelconacaranay@gmail.com>
2023-09-28 03:37:36 +0800

    search filter complete

--- src/actions/getListings.ts
+++ src/actions/getListings.ts
@@ -24,63 +24,42 @@ export default async function getListings(params: IListingsParams) {
       category,
     } = params;
 
-    let query: any = {};
-
-    if (userId) {
-      query.userId = userId;
-    }
-
-    if (category) {
-      query.category = category;
-    }
-
-    if (roomCount) {
-      query.roomCount = {
-        gte: +roomCount,
-      };
-    }
-
-    if (guestCount) {
-      query.guestCount = {
-        gte: +guestCount,
-      };
-    }
-
-    if (bathroomCount) {
-      query.bathroomCount = {
-        gte: +bathroomCount,
-      };
-    }
-
-    if (locationValue) {
-      query.locationValue = locationValue;
-    }
-
-    if (startDate && endDate) {
-      query.NOT = {
-        reservations: {
-          some: {
-            OR: [
-              {
-                endDate: { gte: startDate },
-                startDate: { lte: startDate },
-              },
-              {
-                startDate: { lte: endDate },
-                endDate: { gte: endDate },
-              },
-            ],
-          },
-        },
-      };
-    }
-
     console.log("====================================");
-    console.log("query", query);
+    console.log("params", params);
     console.log("====================================");
 
     const listings = await prisma.listing.findMany({
-      where: query,
+      // where: query,
+      where: {
+        userId: userId || undefined,
+        category: category || undefined,
+        roomCount: {
+          gte: Number(roomCount) || undefined,
+        },
+        guestCount: {
+          gte: Number(guestCount) || undefined,
+        },
+        bathroomCount: {
+          gte: Number(bathroomCount) || undefined,
+        },
+        locationValue: locationValue || undefined,
+        NOT: {
+          reservations: {
+            some: {
+              OR: [
+                {
+                  endDate: { gte: startDate },
+                  startDate: { lte: startDate },
+                },
+                {
+                  startDate: { lte: endDate },
+                  endDate: { gte: endDate },
+                },
+              ],
+            },
+          },
+        },
+      },
       orderBy: {
         createdAt: "desc",
       },
